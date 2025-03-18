005e766d070b24b337bd9402df004d6d0b6b7ff1
User <User@BP20230309>
2025-03-18 09:58:19 -0300

    Finalização do básico do aplicativo - remover e selecionar itens da lista

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -36,7 +36,7 @@ namespace MauiAppMinhasCompras.Helpers
 
         public Task<List<Produto>> Search(string q) 
         {
-            string sql = "SELECT * Produto WHERE DESCRICAO like '%" + q + "%'";
+            string sql = "SELECT * FROM Produto WHERE DESCRICAO like '%" + q + "%'";
 
             return _conn.QueryAsync<Produto>(sql);
         }
